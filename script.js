efb085367f0eea2b13e3aa08f50e02575ad7a51d
Aidana K <aidana.amnbk@gmail.com>
2024-03-21 19:14:36 +0500

    info: need to add aditional features in future

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const app = new App();
+
+// ability to change training parameters
+// ability to delete training
+// ablity to delete all trainings
+// abiloity to sort trainings in the list
+// recreate Running and Cycling objects after parsing
+// Real error messages after incorrect input
+// Ability to set map in the way to show all trainigns (need to google the library documentation)
+// Abilit to draw lines and figures on the map instead of the dots
+// Geocoding of the location (after async)
+// Show data regarding weather
